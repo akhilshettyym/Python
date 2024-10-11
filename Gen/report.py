9de081554b641264807326e61df61c91ff0b9ca5
AKHIL SHETTY M <akhilshettym@AKHILs-MacBook-Air.local>
2024-10-11 22:02:14 +0530

    Initial coomit of Chapter 6

---
+++ Gen/report.py
@@ -0,0 +1,5 @@
+a = "nrp: a multi-source, heterogeneous, automatic data collection system for infants in neonatal intensive care units ."
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.title())
