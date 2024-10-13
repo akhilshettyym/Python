efd7adc077c949edaf5e22caabd9a9dd3b01878e
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-13 19:17:20 +0530

    Chapter 8 - Quick quiz commit

--- Chapter 8/Quick_quiz.py
+++ Chapter 8/Quick_quiz.py
@@ -3,3 +3,11 @@ i = 1
 while i <=50:
     print(i)
     i = i+1
+
+
+i = 0
+while i < 5: # print "Harry" – 5 times!
+    print("Akhil")
+    i = i + 1    
+
+# Note: If the condition never become false, the loop keeps getting executed.
---
+++ Chapter 8/Quick_quiz_2.py
@@ -0,0 +1,7 @@
+# Quick Quiz: Write a program to print the content of a list using while loops.
+
+list = [1, "Akhil", False, "Khushi", 4, 99, "This", "Lochan"]
+i = 0
+while (i<len(list)):
+    print(list[i])
+    i = i+1
