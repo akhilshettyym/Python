001b966c3233d26cd5cd16af05b5497ef1ce8290
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-13 20:05:08 +0530

    Chap 8 - pass statement

---
+++ Chapter 8/pass.py
@@ -0,0 +1,15 @@
+# PASS STATEMENT
+# pass is a null statement in python. t instructs to “do nothing”.
+
+for i in range(100):
+    pass
+
+i = 0
+while (i<15):
+    print(i)
+    i = i+1
+
+
+# l = [1,7,8]
+# for item in l:
+#     pass # without pass, the program will throw an error
