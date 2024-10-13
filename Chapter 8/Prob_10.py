6ee5de0ddf5142236e9e868b371cd8ee2f552cd8
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-13 21:32:21 +0530

    Chap 8 - Practice set - Prob 10

---
+++ Chapter 8/Prob_10.py
@@ -0,0 +1,20 @@
+# 10. Write a program to print multiplication table of n using for loops in reversed order.
+
+n = int(input("Enter a number :"))
+for i in range(1, 11):
+    print(f"{n} X {11-i} = {n*(11-i)}")
+
+'''
+1   10
+2   9
+3   8
+4   7
+5   6
+6   5   
+7   4
+8   3
+9   2
+10  1
+
+sum of these will be 11 so subtract i by 11
+'''
