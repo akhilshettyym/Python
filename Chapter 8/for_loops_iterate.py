5bb4d5f2d5b19a3e22b6c94574160b60786e7b3e
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-13 19:38:24 +0530

    Chap 8 - For loop iterates

---
+++ Chapter 8/for_loops_iterate.py
@@ -0,0 +1,17 @@
+# For loop with list
+list = [1, 34, 567, 775, 45, 765, 12]
+for i in list:
+    print(i)
+print("For loop using LIST")
+
+# For loop with tuple
+tuple = (1, 4 , 765, 43 , 533, 435, 344)
+for i in tuple:
+    print(i)
+print("For loop using TUPLE")
+
+# For loop with string
+string = "Akhil"
+for i in string:
+    print(i)
+print("For loop for string")
