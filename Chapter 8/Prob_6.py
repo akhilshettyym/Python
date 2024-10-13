da40131d1c9e86b3d3b04eeaa988f5995ec8dc83
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-13 20:38:21 +0530

    Chap 8 - Practice set - Prob 6

---
+++ Chapter 8/Prob_6.py
@@ -0,0 +1,15 @@
+# 6. Write a program to calculate the factorial of a given number using for loop.
+
+
+n = int(input("Enter a number: "))
+f = 1
+for i in range(1, n + 1):
+    f *= i
+    print("Factorial of", n, "is:", f)  
+
+
+n = int(input("Enter a number: "))
+f = 1
+for i in range(1, n+1):
+    f = f * i
+    print(f"Factorial of {n} is {f}")
