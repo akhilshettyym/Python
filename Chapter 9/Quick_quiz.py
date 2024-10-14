1a5a00125b5a7e60db64f306a4e0c10b9ae5d910
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-14 21:43:38 +0530

    Chap 9 - Quick_quiz.py

---
+++ Chapter 9/Quick_quiz.py
@@ -0,0 +1,11 @@
+# Quick Quiz: Write a program to greet a user with “Good day” using functions.
+
+def greet():
+    user = input("What is your name :")
+    print("Good day", user)
+greet()
+
+def greetings():
+    user = input("What is your name :")
+    print(f"Good Day {user}")
+greetings()
