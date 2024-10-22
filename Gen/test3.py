7256b47e311dad824a71a3b714d74f160392f583
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-22 21:02:32 +0530

    Gen - test3

--- Gen/test3.py
+++ Gen/test3.py
@@ -1,2 +1,12 @@
 total = sum(range(1, 11))
 print(total)
+
+
+# Input a number
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
