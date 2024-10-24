eb9849dec8a696489000fabdef625e0369286f14
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-24 23:14:31 +0530

    Gen - largest number

---
+++ Gen/largest.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+# Check which number is largest
+largest = max(a, b, c)
+print(f"The largest number is: {largest}")
