15ff0668914eacb394849531e6615e63ec857851
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-31 12:33:07 +0530

    Chapter 9 recursion

---
+++ Chapter 9/Recursion.py
@@ -0,0 +1,11 @@
+"""
+factorial(1) = 1
+factorial(2) = 2 X 1
+factorial(3) = 3 X 2 X 1
+factorial(4) = 4 X 3 X 2 X 1
+factorial(5) = 5 X 4 X 3 X 2 X 1
+
+factorial(n) = n X n-1 X ... X 3 X 2 X 1
+
+factorial(n) = n * factorial(n-1)
+"""
