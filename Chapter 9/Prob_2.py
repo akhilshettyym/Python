739e4f29a5262078a9099d90852ee81ff11806da
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-31 13:10:44 +0530

    Chapter 9 fahrenheit to celsius

--- Chapter 9/Prob_2.py
+++ Chapter 9/Prob_2.py
@@ -1,8 +1,18 @@
 # 2. Write a python program using function to convert Celsius to Fahrenheit.
-
+# c/5 = f-32/9
 f = int(input("Enter temperature in Celsius :"))
 def Fahrenheit(f):
-    c = 5 * (f-32)/9
+    c = (f - 32) * 5/9
     return c
 print("Temperature in Fahrenheit is : ",Fahrenheit(f))
 
+# Write a python program using function to convert Fahrenheit to Celsius.
+c = int(input("Enter the temperature in Fahrenheit :"))
+def Celsius(c):
+    f = (9/5 * c) + 32
+    return f
+print("Temperature in Celsius is : ", Celsius(c))
+
+
+# °C = (°F - 32) × 5/9
+# °F = (9/5 × °C) + 32.
