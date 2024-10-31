de0e80b2c00081d693152bd183b7b2c116eb04a5
akhilshettyym <akhilshettym2003@gmail.com>
2024-10-31 21:43:55 +0530

    Print n lines of patterns

---
+++ Chapter 9/Prob_5.py
@@ -0,0 +1,23 @@
+"""
+5. Write a python function to print first n lines of the following pattern:
+***
+** - for n = 3
+*
+"""
+
+def pattern(n):
+    if n == 0:
+        return
+    print("*" * n)
+    pattern(n - 1)
+pattern(5)
+
+
+# Input number 
+def pattern(n):
+    if n == 0:
+        return
+    print("*" * n)
+    pattern(n - 1)
+n = int(input("Enter a number :"))
+print(pattern(n))
