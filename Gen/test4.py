ab1f99b9e9470bfea205283222d4530b5d7fbde4
akhilshettyym <akhilshettym2003@gmail.com>
2024-11-06 13:04:35 +0530

    Simple calculator

---
+++ Gen/test4.py
@@ -0,0 +1,20 @@
+# Simple Calculator Program
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+if num2 == 0:
+    print("0")
+else:
+    division = num1 / num2 
+
+
+# Print the results
+print("\nResults:")
+print(f"{num1} + {num2} = {addition}")
+print(f"{num1} - {num2} = {subtraction}")
+print(f"{num1} * {num2} = {multiplication}")
+print(f"{num1} / {num2} = {division}")
