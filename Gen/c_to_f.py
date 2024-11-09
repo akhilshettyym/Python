a28eb63e05f88ea7e27705a478152950b0fb9397
akhilshettyym <akhilshettym2003@gmail.com>
2024-11-09 20:25:15 +0530

    Degree to fahrenheit

---
+++ Gen/c_to_f.py
@@ -0,0 +1,4 @@
+# Program to convert Celsius to Fahrenheit
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C is equal to {fahrenheit}°F.")
