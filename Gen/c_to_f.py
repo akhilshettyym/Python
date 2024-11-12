d368a10cc14830413bcd138c0d71cff296a3801a
akhilshettyym <akhilshettym2003@gmail.com>
2024-11-12 23:03:51 +0530

    Degree to fahrenheit

--- Gen/c_to_f.py
+++ Gen/c_to_f.py
@@ -2,3 +2,11 @@
 celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 9/5) + 32
 print(f"{celsius}°C is equal to {fahrenheit}°F.")
+# Program to convert Fahrenheit to Celsius
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print(f"{fahrenheit}°F is equal to {celsius}°C.")
+# Program to convert Celsius to Kelvin
+celsius = float(input("Enter temperature in Celsius: "))
+kelvin = celsius + 273.15
+print(f"{celsius}°C is equal to {kelvin}K.")
