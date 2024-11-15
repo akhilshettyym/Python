7f288d6c244edfce640ac9d0f74403aeb260660f
akhilshettyym <akhilshettym2003@gmail.com>
2024-11-15 21:30:14 +0530

    Reading txt from a given file

---
+++ Chapter 10/Prob_1.py
@@ -0,0 +1,10 @@
+# Write a program to read the text from a given file ‘poems.txt’ and find out
+# whether it contains the word ‘twinkle’.
+
+f = open("Chapter 10/poem.txt")
+content = f.read()
+if ("Twinkle" in content):
+    print("The word 'twinkle' is present in the file.")
+else:
+    print("The word 'twinkle' is not present in the file.")
+f.close()
